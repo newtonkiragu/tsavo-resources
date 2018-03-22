d9688e165a67b9831b38ed3b25ebe8e6b0cbf4d1
Karanu Newton <monster@localhost.localdomain>
2018-03-22 17:18:39 +0300

    removed echo when receiving name from user

--- cookies_for_sessions.php
+++ cookies_for_sessions.php
@@ -7,7 +7,7 @@ function tsavo_user() {
 		} else {
 			echo $_COOKIE["tsavo_user"];
 			$cookie_name = "tsavo_user";
-			$cookie_value = echo $_POST['wpforms-2703-field_1'];;
+			$cookie_value = $_POST['wpforms-2703-field_1'];;
 			if (!isset($_COOKIE)) {
 			setcookie( $cookie_name, $cookie_value, 3 * DAYS_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN );
 		} else {
